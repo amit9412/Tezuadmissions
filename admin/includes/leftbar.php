0f7359965314829d8a3ab680484972efc505b65f
csm21041 <ketan8899.41@gmail.com>
2023-06-03 18:38:03 +0530

    department addition

--- admin/includes/leftbar.php
+++ admin/includes/leftbar.php
@@ -4,14 +4,21 @@
         <li class=" nav-item"><a href="dashboard.php"><i class="la la-home"></i><span class="menu-title" data-i18n="nav.dash.main">Dashboard</span></a>
         </li>
 
-     
+        <li class=" nav-item"><a href="#"><i class="la la-file"></i><span class="menu-title" data-i18n="nav.footers.main">Departments</span></a>
+          <ul class="menu-content">
+            <li><a class="menu-item" href="add-course.php" data-i18n="nav.footers.footer_light">Add Department</a>
+            </li>
+            <li><a class="menu-item" href="manage-course.php" data-i18n="nav.footers.footer_dark">Manage Department</a>
+            </li>
+          </ul>
+        </li> 
+
      <li class=" nav-item"><a href="#"><i class="la la-file"></i><span class="menu-title" data-i18n="nav.footers.main">Course</span></a>
           <ul class="menu-content">
             <li><a class="menu-item" href="add-course.php" data-i18n="nav.footers.footer_light">Add Course</a>
             </li>
             <li><a class="menu-item" href="manage-course.php" data-i18n="nav.footers.footer_dark">Manage Course</a>
             </li>
-         
           </ul>
         </li> 
            <li class=" nav-item"><a href="user-detail.php"><i class="la la-users"></i><span class="menu-title" data-i18n="nav.dash.main">Reg. Users</span></a>
