af621a4374e682888924c8cc05a9012d97b1f240
csm21041 <ketan8899.41@gmail.com>
2023-06-05 00:12:14 +0530

    Update dbconnection.php

--- department/includes/dbconnection.php
+++ department/includes/dbconnection.php
@@ -3,6 +3,6 @@ $con=mysqli_connect("localhost", "root", "", "camsdb");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
-mysqli_report(MYSQLI_REPORT_ALL);
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 mysqli_error($con);
   ?>
